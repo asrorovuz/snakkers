c40a7a6e3c682819245517c3adeed0894a1d1c4c
Asrorov Sardor <asrorovsardor07@gmail.com>
2023-04-13 23:23:53 +0500

    add header component

---
+++ src/components/app/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../header/Header'
+import './App.css'
+
+export const App = () => {
+  return (
+    <div className='App container'>
+      <Header />
+      <main>
+      </main>
+    </div>
+  )
+}
+
