b2241114032c4f30671224d116f3d6984ca1572b
Asrorov Sardor <asrorovsardor07@gmail.com>
2023-05-06 20:06:48 +0500

    add product card

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Banner from '../banner/Banner'
 import Basket from '../basket/Basket'
 import Header from '../header/Header'
+import MainCard from '../main/MainCard'
 import './App.css'
 
 export const App = () => {
@@ -24,6 +25,7 @@ export const App = () => {
       <main className='main container'> 
         <Header showBasket={showBasket}/> 
         <Banner />
+        <MainCard />
       </main>
     </div>
   )
---
+++ src/components/main/MainCard.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import "./MainCard.css";
+import searchIcon from "../../assistant/search.png";
+import Like from '../../assistant/like.png';
+import LightLike from '../../assistant/light-like.png';
+import { useState } from "react";
+import boots from '../../assistant/shoose/1.png'
+import boots2 from '../../assistant/shoose/2.png'
+
+const MainCard = () => {
+    const [like, setLike] = useState(false)
+
+    const changeLike = () => {
+        setLike(!like)
+    }
+
+  return (
+    <div className="main">
+      <div className="d-flex card-header">
+        <h2>Все кроссовки</h2>
+        <div className="d-flex search-block">
+          <label htmlFor="search">
+            <img src={searchIcon} alt="search icon" />
+          </label>
+          <input type="search" placeholder="Поиск..." id="search"/>
+        </div>
+      </div>
+      <div className="d-flex cards">
+        <div className="card">
+            <div className="card-like" onClick={changeLike}>
+                <img src={like ? Like : LightLike} alt="like icon" />
+            </div>
+            <div className="card-img">
+                <img src={boots} alt="boots img" />
+            </div>
+        </div>
+        <div className="card">
+            <div className="card-like" onClick={changeLike}>
+                <img src={like ? Like : LightLike} alt="like icon" />
+            </div>
+            <div className="card-img">
+                <img src={boots2} alt="boots img" />
+            </div>
+        </div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+      </div>
+    </div>
+  );
+};
+
+export default MainCard;
